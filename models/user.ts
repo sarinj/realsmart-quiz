690621b62f88bb5a1f6fe71022262d00bfd82b95
Sarinj <sarin19222@gmail.com>
2024-08-22 15:27:35 +0700

    fix user shcemas

--- models/user.ts
+++ models/user.ts
@@ -4,7 +4,7 @@ const userSchema = new Schema(
   {
     name: { type: String, required: true },
     username: { type: String, required: true },
-    password: { type: String, default: true },
+    password: { type: String, required: true },
   },
   { timestamps: true }
 )
